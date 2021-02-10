c8e07f6bbb8aba4869d235717d39d2e1bbc150c7
Danylo Kaminskyi <DanyloKam@gmail.com>
2021-02-10 16:48:17 +0200

    Added working Repo

--- src/CommandApi/Data/CommandContext.cs
+++ src/CommandApi/Data/CommandContext.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommandAPI.Data
 {
-    public class CommandContext
+    public class CommandContext : DbContext
     {
+        public CommandContext(DbContextOptions<CommandContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Command> CommandItems { get; set; }
     }
 }
