20a42cf5c4d62bf8ff9c5e68210effa1104d2686
Danylo Kaminskyi <DanyloKam@gmail.com>
2021-02-22 10:00:07 +0200

    added unit tests

---
+++ test/CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using CommandAPI.Models;
+using Xunit;
+
+namespace CommandAPI.Tests
+{
+    public class CommandTests :IDisposable
+    {
+        Command testCommand;
+
+        public CommandTests()
+        {
+            testCommand = new Command
+            {
+                HowTo = "Do something",
+                Platform = "Some Platform",
+                CommandLine = "Some Command Line"
+            };
+        }
+
+        public void Dispose()
+        {
+            testCommand = null;
+        }
+
+        [Fact]
+        public void CanChangeHowTo()
+        {
+
+            //Act
+            testCommand.HowTo = "Execute unit tests";
+            
+            //Assert
+            Assert.Equal("Execute unit tests", testCommand.HowTo);
+        }
+
+        [Fact]
+        public void CanChangePlatform()
+        {
+
+            //Act
+            testCommand.Platform = "XUnit";
+
+            //Assert
+            Assert.Equal("XUnit", testCommand.Platform);
+        }
+
+        [Fact]
+        public void CanChangeCommandLine()
+        {
+            //Act
+            testCommand.CommandLine= "dotnet test";
+
+            //Assert
+            Assert.Equal("dotnet test", testCommand.CommandLine);
+        }
+    }
+}
