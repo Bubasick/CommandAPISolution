c4d6f089fd4251bac23cad1668a5fe3b9ab27f2f
Danylo Kaminskyi <DanyloKam@gmail.com>
2021-02-25 11:45:47 +0200

    fix fuckups

---
+++ src/CommandApi/Data/SqlCommandApiRepo.cs
@@ -0,0 +1,56 @@
+﻿using CommandAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommandAPI.Data
+{
+    public class SqlCommandApiRepo : ICommandAPIRepo
+    {
+        private readonly CommandContext _context;
+
+        public SqlCommandApiRepo(CommandContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateCommand(Command cmd)
+        {
+            if (cmd == null)
+            {
+                throw new ArgumentException(nameof(cmd));
+            }
+
+            _context.CommandItems.Add(cmd);
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            if (cmd == null)
+            {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+
+            _context.CommandItems.Remove(cmd);
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _context.CommandItems.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _context.CommandItems.FirstOrDefault(x => x.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+        }
+    }
+}
